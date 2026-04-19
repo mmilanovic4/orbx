b827790ae6837a163844cfa124ccac2cb5413dfe
Miloš Milanović <mmilanovic016@gmail.com>
2026-04-19 17:53:53 +0200

    chore: add xkcd command

---
+++ cmd/xkcd.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+type XKCD struct {
+	Title string `json:"title"`
+	Img   string `json:"img"`
+	Alt   string `json:"alt"`
+	Num   int    `json:"num"`
+}
+
+var xkcdCmd = &cobra.Command{
+	Use:   "xkcd",
+	Short: "Fetch latest XKCD comic",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		resp, err := http.Get("https://xkcd.com/info.0.json")
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		var comic XKCD
+		if err := json.NewDecoder(resp.Body).Decode(&comic); err != nil {
+			fmt.Println("Decode error:", err)
+			return
+		}
+
+		fmt.Printf("\n#%d - %s\n", comic.Num, comic.Title)
+		fmt.Println("Image:", comic.Img)
+		fmt.Println("\nAlt text:")
+		fmt.Println(comic.Alt)
+
+		exec.Command("open", comic.Img).Start()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(xkcdCmd)
+}
